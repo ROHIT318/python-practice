f67e406ae9ec03163fb6f88e8143d864659afa3b
rohit318 <iro1hit@gmail.com>
2023-07-30 21:47:55 +0530

    Added basic functions file

---
+++ basic_functions.py
@@ -0,0 +1,13 @@
+# Print function
+# It is a built-in function used to display output or information to the console or standard output stream.
+print(arg1, arg2, arg3, ...., sep=' ', end='\n', file=sys.stdout, flush=False)
+# arg1, arg2, arg3, ....: The values to be printed
+# sep: To separate the arguements. Optional, default=' '
+# end: Character to be printed at the end of statement. Optional, default='\n'
+# flush: To force the output to flush immediately or not. Optional, default=False 
+
+# Input function
+# It  is a built-in function used to read user input from the console.
+# var = input(prompt)
+# prompt: The string argument provided to input() that serves as the prompt displayed to the user.
+
