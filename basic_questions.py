6720cb16e7687d1e33ce452e7caf871eb90f6506
rohit318 <iro1hit@gmail.com>
2023-08-13 21:07:12 +0530

    More practiced solutions

--- basic_questions.py
+++ basic_questions.py
@@ -29,3 +29,43 @@ if cal_num == num:
 else:
     print("Random Number .....")
 # 2) Whether an armstrong number? (ends)
+
+
+# 3) Calculate radius of a circle (starts)
+rad = 5
+pi = 3.141
+area = pi * (rad**2)
+print(area)
+# 3) Calculate radius of a circle (ends)
+
+# 4) Genarate a list from a string separated by commas (starts)
+var = "1,2,3,4"
+var_list = var.split(",")
+print(var_list)
+print(type(var_list))
+# 4) Genarate a list from a string separated by commas (ends)
+
+# 5) Write a Python program that accepts a filename from the user and prints the extension of the file. (starts)
+file_name = "some file.cpp"
+file_ext = file_name.split(".")
+print(file_ext[-1])
+# 5) Write a Python program that accepts a filename from the user and prints the extension of the file. (ends)
+
+# 6) To insert element in the middle of the list (starts)
+list = [0,1,2,4,5]
+print(list)
+to_ins = 3
+index_num = len(list)//2 + 1
+list.insert(index_num,3)
+print(list)
+# 6) To insert element in the middle of the list (ends)	
+
+# 7) Difference between number and 100, negative result then twice and absolute of result otherwise difference itself. (starts)
+var = 10
+diff = var - 100
+
+if diff > 0:
+    print(diff)
+else:
+    print(abs(diff)*2)
+# 7) Difference between number and 100, negative result then twice and absolute of result otherwise difference itself. (ends)
