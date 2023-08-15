fb33248460bed746c1fcae0bd065fae1a432714b
rohit318 <iro1hit@gmail.com>
2023-08-15 21:32:46 +0530

    Added file handling code

--- os_module.py
+++ os_module.py
@@ -18,3 +18,24 @@ os.chdir("DATA/")
 print(os.getcwd())
 
 print(os.path.exists("data"))
+
+# Get OS information name
+print(os.name)
+print(os.uname())
+
+# Get file directories information
+for files in os.scandir('/home/'):
+    if files.is_dir() == True:
+        print("{} is a directory".format(files.name))
+    else:
+        print("{} is a file".format(files.name))
+
+# To create a file 
+os.mkdir("lol.txt")
+print(os.listdir())
+
+# To read and write in a file
+with open('/home/lol.txt', 'w') as content:
+    content.write("This is file whose name is lol.txt; LOL")
+with open('/home/lol.txt', 'r') as content:
+    print(content.read())
