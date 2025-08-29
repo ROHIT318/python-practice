4e0ea649a0cd84696a7ae6674575e5a724e1d411
ROHIT318 <iro1hit@gmail.com>
2025-08-29 21:28:01 +0530

    Added documentation

--- apis_implementation/outlook_mails.py
+++ apis_implementation/outlook_mails.py
@@ -8,8 +8,21 @@ import os
 load_dotenv()
 
 class OutlookMail():
+    """
+    Use to create instance which gives mail receiving date, sender mail and mail subject.
+    """
 
     def __init__(self, Authorization: str, start_date: datetime, end_date: datetime, num_of_msgs: int=5, content_type: str='application/json') -> None:
+        """
+        Inputs:
+            Authorization: Your access token. Ex: 'Bearer <your-token>'
+            start_date: Date from which to fetch the mail. Ex: 'YYYY-MM-DDTHH:MM:SSZ'
+            end_date: Date up till which to fetch the mail. Ex: 'YYYY-MM-DDTHH:MM:SSZ'
+            num_of_msgs: Number of mails to fetch. Default value is 5. Ex: '5|6|7....'
+            content_type
+        Output:
+            None: Creates instance of a class
+        """
         self.Authorization = Authorization
         self.content_type = content_type
         self.start_date = start_date
@@ -19,6 +32,12 @@ class OutlookMail():
         self.num_of_msgs = num_of_msgs
     
     def create_request(self) -> str:
+        """
+        Input:
+            None
+        Output:
+            Returns API url created on the basis of self.start_date and self.end_date
+        """
         created_url = (
             f"https://graph.microsoft.com/v1.0/me/messages"
             f"?$filter=receivedDateTime ge {self.start_date} and receivedDateTime le {self.end_date}"
@@ -26,6 +45,12 @@ class OutlookMail():
         return created_url
     
     def return_messages(self) -> pd.DataFrame:
+        """
+        Input:
+            None.
+        Output:
+            Returns a dataframe consisting of 3 columns. receiving date, email address and mail subject.
+        """
         response = requests.get(url=self.url, headers=self.headers)
         messages = response.json()
         # return messages
